b106e5abb81f874f8e762e1dc6cd38f5afe54dc1
Vana Brajcic <vana.brajcic@gmail.com>
2018-02-21 10:18:43 +0100

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,21 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import rootReducer from './reducers';
 
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from '../sagas/sagas';
+
 export default function configureStore(initialState) {
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const sagaMiddleware = createSagaMiddleware();
+
   const store = createStore(
     rootReducer,
     initialState,
     composeEnhancers(
-      applyMiddleware(),
+      applyMiddleware(sagaMiddleware),
   ));
+
+  sagaMiddleware.run(rootSaga);
 
   return store;
 }
